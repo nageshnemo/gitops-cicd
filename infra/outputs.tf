f3f22ddaa87ea3c2d39557419ac4f9ee0f05dd02
Nagesh <nagesh@Nageshs-MacBook-Air.local>
2024-11-24 22:26:29 +0530

    adding files

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,11 +1,7 @@
 output "gke_cluster_name" {
-  value = module.gke.name
+  value = module.gke_cluster.name
 }
 
 output "gke_endpoint" {
-  value = module.gke.endpoint
-}
-output "artifact_registry_repo_url" {
-  description = "Docker Artifact Registry URL"
-  value       = "https://${var.region}-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.docker_repo.repository_id}"
+  value = module.gke_cluster.endpoint
 }
